2c5a391f856be8e0bbe2d6a1d4bbe0557c1734e9
Murali Karthik <muralikarthikbeeram07@gmail.com>
2026-03-21 15:33:39 +0530

    formating

---
+++ pythonBasicsPt3/formating.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+sum = a + b
+
+#normal formating
+print("sum of {} and {} is {}".format(a, b, sum))
+
+#value based formating
+print("values of vars {} & {} ".format(a, b))
+
+#F-strings
+print(f"sum of {a} and {b} is {sum}")
+
