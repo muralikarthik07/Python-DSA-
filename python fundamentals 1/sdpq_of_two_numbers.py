036032121ffdc3b826681f8a6d29da37ea678e9d
Karthik <muralikarthikbeeram07@gmail.com>
2026-06-01 14:44:17 +0530

    sdpq of two numbers

---
+++ python fundamentals 1/sdpq_of_two_numbers.py
@@ -0,0 +1,7 @@
+a = input("enter a number: ")
+b = input("enter b number: ")
+
+print("sum of there num is: ", int(a) + int(b))
+print("difference of there num is : ", int(a) - int(b))
+print("product of there num is : ", int(a) * int(b))
+print("quotient of there num is : ", int(a) / int(b))
