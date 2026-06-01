862b146c01b17e5d131ec4c10adc19e16bf634d1
Karthik <muralikarthikbeeram07@gmail.com>
2026-06-01 14:49:53 +0530

    num float avg

---
+++ python fundamentals 1/num_float_average.py
@@ -0,0 +1,7 @@
+a = input("enter a number: ")
+b = input("enter another number: ")
+c = float(input("enter a third number: "))
+
+avg = (float(a) + float(b) + c)/ 3
+
+print("the avg of 3 numbers is: ", avg)
